1294c9bdfbfb36ba683fde5decec9651b9a17d8e
ParkerBacall <pbacall2@gmail.com>
2020-01-11 11:15:11 -0700

    heroku config

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_01_10_180040) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clicks", force: :cascade do |t|
     t.integer "number"
     t.datetime "created_at", precision: 6, null: false
