980b6f07b9dd489415f069942c9eedf19851d393
ParkerBacall <pbacall2@gmail.com>
2020-01-13 18:06:30 -0700

    reset

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'clickboi12.firebaseapp.com/'
 
-    resource '*',
+    resource 'clickboi12.firebaseapp.com',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
